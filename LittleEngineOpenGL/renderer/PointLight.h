93520bd00fb91fe8c028a1dd94c3e6523b34794f
Mateusz Rzeczyca <mateusz.rzeczyca@nokia.com>
2022-06-07 21:25:30 +0200

    added point light class

---
+++ LittleEngineOpenGL/renderer/PointLight.h
@@ -0,0 +1,33 @@
+
+#ifndef POINTLIGHT_H
+#define POINTLIGHT_H
+
+
+#include "includes.h"
+#include "defines.h"
+#include "math/vec3.h"
+
+
+namespace le
+{
+
+
+
+struct PointLightSpecification {
+	vec3 position{ 0.f, 0.f, 0.f };
+};
+
+
+class PointLight {
+public:
+
+private:
+
+
+};
+
+
+}
+
+
+#endif
