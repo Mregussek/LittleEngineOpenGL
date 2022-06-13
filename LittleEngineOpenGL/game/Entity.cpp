a7f308e629e7a8b073148ded82449c0c7283ff94
Mateusz Rzeczyca <mateusz.rzeczyca@nokia.com>
2022-06-13 23:55:21 +0200

    created interesting parking

--- LittleEngineOpenGL/game/Entity.cpp
+++ LittleEngineOpenGL/game/Entity.cpp
@@ -9,19 +9,21 @@ namespace le
 
 void ParkingEntity::start() {
     const rotation3 frontRot{ 1.f, 1.f, 1.f };
-    const rotation3 rightRot{ 0.f, 1.f, 0.f };
+    const rotation3 yAxisRot{ 0.f, 1.f, 0.f };
     const scale3 defaultScale{ 0.2f, 0.2f, 0.2f };
-    const f32 defaultAngle{ 0.f };
-    const f32 rightAngle90{ -90.f };
-    mPlaceVector.add<StartPlace>(point3{ 0.f, 0.f, 0.f }, rotation3{ frontRot }, scale3{ defaultScale }, Colors::blue(ColorType::DEFAULT), defaultAngle);
-    mPlaceVector.add<Road01Place>(point3{ 0.f, 0.f, -2.f }, rotation3{ frontRot }, scale3{ defaultScale }, Colors::red(ColorType::DEFAULT), defaultAngle);
-    // mPlaceVector.add<Road01Place>(point3{ 2.f, 0.f, -2.f }, rotation3{ frontRot }, scale3{ defaultScale }, Colors::blue(ColorType::DEFAULT), defaultAngle);
-    mPlaceVector.add<Road02Place>(point3{ 0.f, 0.f, -4.f }, rotation3{ frontRot }, scale3{ defaultScale }, Colors::yellow(ColorType::DEFAULT), defaultAngle);
-    // mPlaceVector.add<Road02Place>(point3{ 2.f, 0.f, -4.f }, rotation3{ frontRot }, scale3{ defaultScale }, Colors::pink(ColorType::DEFAULT), defaultAngle);
-    mPlaceVector.add<Road03Place>(point3{ 0.f, 0.f, -6.f }, rotation3{ frontRot }, scale3{ defaultScale }, Colors::blue(ColorType::DEFAULT), defaultAngle);
-    // mPlaceVector.add<Road03Place>(point3{ 2.f, 0.f, -6.f }, rotation3{ frontRot }, scale3{ defaultScale }, Colors::red(ColorType::DEFAULT), defaultAngle);
-    mPlaceVector.add<ParkingPlace>(point3{ 0.f, 0.f, -8.f }, rotation3{ frontRot }, scale3{ defaultScale }, Colors::pink(ColorType::DEFAULT), defaultAngle);
-    // mPlaceVector.add<ParkingPlace>(point3{ 2.f, 0.f, -8.f }, rotation3{ frontRot }, scale3{ defaultScale }, Colors::yellow(ColorType::DEFAULT), defaultAngle);
+    mPlaceVector.add<StartPlace>(point3{ 0.f, 0.f, 0.f }, rotation3{ frontRot }, scale3{ defaultScale }, Colors::yellow(ColorType::DEFAULT), 0.f);
+    mPlaceVector.add<Road01Place>(point3{ 0.f, 0.f, -2.f }, rotation3{ frontRot }, scale3{ defaultScale }, Colors::red(ColorType::DEFAULT), 0.f);
+
+    mPlaceVector.add<Road03Place>(point3{ 0.f, 0.f, -4.f }, rotation3{ yAxisRot }, scale3{ defaultScale }, Colors::blue(ColorType::DEFAULT), -90.f);
+
+    mPlaceVector.add<Road02Place>(point3{ 2.f, 0.f, -4.f }, rotation3{ yAxisRot }, scale3{ defaultScale }, Colors::yellow(ColorType::DEFAULT), 90.f);
+    mPlaceVector.add<Road02Place>(point3{ 4.f, 0.f, -4.f }, rotation3{ yAxisRot }, scale3{ defaultScale }, Colors::yellow(ColorType::DEFAULT), 90.f);
+
+    mPlaceVector.add<Road03Place>(point3{ 6.f, 0.f, -4.f }, rotation3{ yAxisRot }, scale3{ defaultScale }, Colors::blue(ColorType::DEFAULT), 90.f);
+
+    mPlaceVector.add<ParkingPlace>(point3{ 2.f, 0.f, -6.f }, rotation3{ yAxisRot }, scale3{ defaultScale }, Colors::pink(ColorType::DEFAULT), 180.f);
+    mPlaceVector.add<ParkingPlace>(point3{ 4.f, 0.f, -6.f }, rotation3{ yAxisRot }, scale3{ defaultScale }, Colors::pink(ColorType::DEFAULT), 180.f);
+    mPlaceVector.add<ParkingPlace>(point3{ 6.f, 0.f, -6.f }, rotation3{ yAxisRot }, scale3{ defaultScale }, Colors::pink(ColorType::DEFAULT), 180.f);
 }
 
 void ParkingEntity::update(f32 deltaTime) {
